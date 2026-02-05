edddf5ca9233c9b580f0b639dd8e8c0903d1d22c
Prasa100 <th3r3al3mail@gmail.com>
2026-02-05 19:49:39 +0530

    Auth works

--- app/page.js
+++ app/page.js
@@ -2,9 +2,12 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    
-    <div>
-      <Button variant="primary" size="xl">Heie sodsanfonkn  </Button>
+    <div className="min-h-screen bg-stone-50 text-stone-900">
+      <section>
+        <Button variant="primary" size="xl">
+          Heie sodsanfonkn{" "}
+        </Button>
+      </section>
     </div>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+import React from "react";
+import {
+  SignedIn,
+  SignInButton,
+  SignedOut,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        Logo
+        <div>Nav Links</div>
+
+        <div className="flex items-center space-x-4">
+          {/* SIGNED OUT STATE */}
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button
+                variant="ghost"
+                className="text-stone-600 hover:text-orange-600 hover:bg-orange-50 font-medium"
+              >
+                Sign In
+              </Button>
+            </SignInButton>
+
+            <SignUpButton mode="modal">
+              <Button variant="primary" className="rounded-full px-6">
+                Get Started
+              </Button>
+            </SignUpButton>
+          </SignedOut>
+
+          {/* SIGNED IN STATE */}
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;

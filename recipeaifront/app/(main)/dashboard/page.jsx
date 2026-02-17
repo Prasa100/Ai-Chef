b56c80a9eff69c4b84698988253dd83c475d9b41
Prasa100 <th3r3al3mail@gmail.com>
2026-02-17 20:08:37 +0530

    add dashboard

---
+++ recipeaifront/app/(main)/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div className="min-h-screen bg-stone-50 py-16 px-4"> Dashboard Page </div>
+  );
+};
+
+export default DashboardPage;
